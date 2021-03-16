d9eef354281f964e554ab8f62c9e42b2a6267e79
Garrett Gibo <garrett.gibo@gmail.com>
2021-03-16 14:03:37 -0700

    Layout components for wavenet

---
+++ wavenet/model.py
@@ -0,0 +1,51 @@
+"""
+Networks and components that define WaveNet
+"""
+import torch
+
+
+class WaveNet(torch.nn.Module):
+    """TODO: class docstring"""
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, data):
+        """TODO: forward pass docstring"""
+        return data
+
+
+class DilatedCauasalConvolution(torch.nn.Module):
+    """TODO: class docstring"""
+
+    def __init__(self):
+        """TODO: forward pass docstring"""
+        super().__init__()
+
+    def forward(self, data):
+        """TODO: forward pass docstring"""
+        return data
+
+
+class GatedActivationUnit(torch.nn.Module):
+    """TODO: class docstring"""
+
+    def __init__(self):
+        """TODO: forward pass docstring"""
+        super().__init__()
+
+    def forward(self, data):
+        """TODO: forward pass docstring"""
+        return data
+
+
+class ResidualBlock(torch.nn.Module):
+    """TODO: class docstring"""
+
+    def __init__(self):
+        """TODO: forward pass docstring"""
+        super().__init__()
+
+    def forward(self, data):
+        """TODO: forward pass docstring"""
+        return data
