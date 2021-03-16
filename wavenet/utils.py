c27ab2f85200f567f112f047c8d97af36bd09be1
Garrett Gibo <garrett.gibo@gmail.com>
2021-03-15 19:39:25 -0700

    Add sampling rate param to standardize converted WAV files

--- wavenet/utils.py
+++ wavenet/utils.py
@@ -15,25 +15,36 @@ def get_data() -> None:
     pass
 
 
-def convert_mp3_folder(input_folder: str) -> str:
+def convert_mp3_folder(input_folder: str, sample_rate: float) -> str:
     """Convert all mp3 files in folder into wav files"""
-    output_folder = create_wav_folder(input_folder)
+    logger.info("Converting mp3 files located in [%s] to WAV with sample rate [%d]",
+                input_folder, sample_rate)
 
+    output_folder = create_wav_folder(input_folder)
     mp3_files = glob.glob(input_folder + "/*.mp3")
 
     for mp3_path in mp3_files:
         mp3_file = mp3_path.split("/")[-1]
-        mp3_to_wav(input_folder, output_folder, mp3_file)
+        mp3_to_wav(input_folder, output_folder, mp3_file, sample_rate)
+
+    logger.info("Converted WAV files located in [%s]", output_folder)
+
+    return output_folder
 
 
-def mp3_to_wav(input_folder: str, output_folder: str, filename: str) -> None:
+def mp3_to_wav(input_folder: str,
+               output_folder: str,
+               filename: str,
+               sample_rate: float) -> None:
     """Convert mp3 file to wav file"""
     mp3_full_path = os.path.join(input_folder, filename)
     wav_full_path = os.path.join(output_folder, filename.replace("mp3", "wav"))
 
     if not os.path.exists(wav_full_path):
         mp3 = AudioSegment.from_mp3(mp3_full_path)
-        mp3.export(wav_full_path, format="wav")
+        mp3.export(wav_full_path,
+                   format="wav",
+                   parameters=["-ar", f"{sample_rate}"])
         logger.debug("Converted %s to wav file", filename)
     else:
         logger.debug("%s already converted to wav file", filename)
