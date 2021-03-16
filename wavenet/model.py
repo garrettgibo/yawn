72884cb840dfc37aa51217307f55265e53050990
Garrett Gibo <garrett.gibo@gmail.com>
2021-03-16 15:53:45 -0700

    Create single residual layer

--- wavenet/model.py
+++ wavenet/model.py
@@ -16,7 +16,36 @@ class WaveNet(torch.nn.Module):
         return data
 
 
-class DilatedCauasalConvolution(torch.nn.Module):
+class ResidualLayer(torch.nn.Module):
+    """Create a single residual layer"""
+
+    def __init__(self, in_channels: int, out_channels: int, dilation: int):
+        """Initialize a residual layer"""
+        super().__init__()
+        self.dilated_causal_conv = DilatedCausalConvolution(
+            in_channels, out_channels, dilation
+        )
+        self.gated_activation = GatedActivationUnit()
+        self.conv = nn.Conv1d(
+            out_channels,
+            out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=False,
+        )
+
+    def forward(self, data):
+        """Pipeline for a single residual layer"""
+        filter_output, gate_output = self.dilated_causal_conv(data)
+        activation_output = self.gated_activation(filter_output, gate_output)
+        skip_output = self.conv(activation_output)
+        output = data + skip_output
+
+        return output, skip_output
+
+
+class DilatedCausalConvolution(torch.nn.Module):
     """Creates a causal dilated convolution layer"""
 
     def __init__(self, in_channels: int, out_channels: int, dilation: int):
@@ -84,15 +113,3 @@ class GatedActivationUnit(torch.nn.Module):
         output = self.tanh(filter_data) * self.sigmoid(gate_data)
 
         return output
-
-
-class ResidualBlock(torch.nn.Module):
-    """TODO: class docstring"""
-
-    def __init__(self):
-        """TODO: forward pass docstring"""
-        super().__init__()
-
-    def forward(self, data):
-        """TODO: forward pass docstring"""
-        return data
