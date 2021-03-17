b405b925aac440f3efb5064a4b13a008fbd5d206
Garrett Gibo <garrett.gibo@gmail.com>
2021-03-16 23:09:11 -0700

    turn mp3 converter into class

--- wavenet/utils/__init__.py
+++ wavenet/utils/__init__.py
@@ -1,3 +1,3 @@
 from .logger import new_logger
 from .train import train
-from .utils import convert_mp3_folder, get_data, load_model, save_model
+from .utils import Mp3Converter, get_data, load_model, save_model
