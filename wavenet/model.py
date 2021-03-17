55493627c8349636573af9ad93d76f34e5cb3c97
Garrett Gibo <garrett.gibo@gmail.com>
2021-03-16 17:03:48 -0700

    Combine components into central architecture

--- wavenet/model.py
+++ wavenet/model.py
@@ -1,6 +1,8 @@
 """
 Networks and components that define WaveNet
 """
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 
@@ -8,7 +10,39 @@ import torch.nn as nn
 class WaveNet(torch.nn.Module):
     """TODO: class docstring"""
 
-    def __init__(self):
+    def __init__(self, in_channels: int, res_channels: int):
+        """Initialize WaveNet.
+
+        Args:
+            in_channels: number of channels for input channel. The number of
+                skip channels is the same as input channels.
+            res_channels: number of channels for residual input and output
+        """
+        super().__init__()
+        self.model = nn.Sequential(
+            OrderedDict(
+                [
+                    ("causal_conv", CausalConv(in_channels, res_channels)),
+                    (
+                        "residual_stack",
+                        ResidualStack(
+                            in_channels=res_channels, out_channels=in_channels
+                        ),
+                    ),
+                    ("conv_1", nn.Conv1d(in_channels, in_channels, kernel_size=1)),
+                    ("relu_1", nn.ReLU()),
+                    ("conv_2", nn.Conv1d(in_channels, in_channels, kernel_size=1)),
+                    ("relu_2", nn.ReLU()),
+                    ("softmax", nn.Softmax()),
+                ]
+            )
+        )
+
+    def forward(self, data):
+        """Forward pass through full architecture"""
+        return self.model(data)
+
+
 class CausalConv(torch.nn.Module):
     """Causal Convolution for WaveNet"""
 
